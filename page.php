f48ab9f50e1c19138f0dd906a70afd481b9f91a8
minnkoaung <minnkoaung@gmail.com>
2015-04-10 12:08:04 +0630

    initial commit

---
+++ page.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='keywords' content='shwepaduk, wedding card, invitation card, printing, myanmar wedding card, myanmar printing, myanmar offset'/>
+    <meta name='description' content='Wedding Card & Printing Servies'/>
+    <link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'>
+    <link rel='icon' href='images/favicon.ico' type='image/x-icon'>
+    <title>Shwe Padauk </title>
+    <link href='css/bootstrap.min.css' rel='stylesheet'>
+    <link href='css/mystyle.css' rel='stylesheet'>
+  </head>
+  <body>
+    <div class='container-fluid fullwrapper'>
+        <div class='container innerwrapper'>
+          <div class='row'>
+              <div class='col-md-2 logowrap'>
+                  <img src='images/logo.png'>
+              </div><!--logowrap-->
+              <div class='col-md-10'>
+                <h1 class='mytitle'>Shwe Padauk</h1>
+                <h4 class='mysubtitle'>Wedding Card & General Printing</h4>
+              </div>
+          </div>
+          <!-- navbar start -->
+            <nav class='navbar navbar-inverse mynav' role='navigation'>
+              <div class='container-fluid'>
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class='navbar-header'>
+                  <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                    <span class='sr-only'>Toggle navigation</span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                  </button>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class='collapse navbar-collapse ' id='bs-example-navbar-collapse-1'>
+                  <ul class='nav navbar-nav'>
+                    <li class=''><a href='#'>Job Data</a></li>
+                    <li><a href='#'>Card Search</a></li>
+                    <li><a href='#'>Gallery</a></li>
+                    <li><a href='#'>Support</a></li>
+                  </ul>
+                  <ul class='nav navbar-nav navbar-right'>
+                    <li class='dropdown'>
+                      <a href='#' class='dropdown-toggle' data-toggle='dropdown'>Admin Login <span class='glyphicon glyphicon-user'></span><b class='caret'></b></a>
+                      <ul class='dropdown-menu'>
+                        <li><a href='#'>User name</a></li>
+                        <li class='divider'></li>
+                        <li><a href='#'><span class='glyphicon glyphicon-log-in'></span> LogIn</a></li>
+                        <li><a href='#'><span class='glyphicon glyphicon-log-out'></span> Logout</a></li>
+                        <li class='divider'></li>
+                        <li><a href='#'>Sign In</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>            
+          <!-- navbar end-->
+        </div><!-- innerwrapper -->
+    </div><!-- fullwrapper end-->
+    <!-- middlewrapper start-->
+    <div class='container-fluid fullwrapper middlewrapper'>
+        <div class='container'>
+          <div class='row'>
+            <h1>Content will comming soon!!</h1>
+          </div>
+        </div>
+    </div>
+    <!-- middlewrapper stop-->
+    <!-- footerwrapper start-->
+    <div class='container-fluid fullwrapper footerwrapper'>
+      <div class='container'>
+      <p class='footertext'>Copyright © Shwe Padauk   <?php   echo date('Y') ?> </p>
+      </div>
+    </div>
+    <!-- footerwrapper stop-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src='js/jquery-1.11.2.min.js'></script>
+    <script src='js/bootstrap.min.js'></script>
+  </body>
+</html>
+
