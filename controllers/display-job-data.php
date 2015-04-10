8746a1452efaf44016b6b42c3e6b84796de9bd8e
minnkoaung <minnkoaung@gmail.com>
2015-04-10 12:34:52 +0630

    object retrived is work!

--- controllers/display-job-data.php
+++ controllers/display-job-data.php
@@ -1,11 +1,12 @@
 <?php
     error_reporting(E_ALL);
 	ini_set("display_errors", 1);
-	include_once "models/Page_Data.class.php";
+	include_once "models/Data_Entry.class.php";
+
     $displayJobData= new Data_Entry($db);
     $diplayData = $displayJobData->displayJobData();
     $onerow = $diplayData->fetchObject();
-    $test = print_r
+    $test = print_r($onerow);
     
   /*  $displayJobData = include_once "views/display-job-data-html.php";
 	return $displayJobData ;*/
