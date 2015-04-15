5a701953c2a6736636c0ac1e89eb3b5251c80175
minnkoaung <minnkoaung@gmail.com>
2015-04-15 22:14:36 +0630

    merg

--- controllers/display-job-data.php
+++ controllers/display-job-data.php
@@ -2,12 +2,12 @@
     error_reporting(E_ALL);
 	ini_set("display_errors", 1);
 	include_once "models/Data_Entry.class.php";
-
     $displayJobData= new Data_Entry($db);
-    $diplayData = $displayJobData->displayJobData();
-    $onerow = $diplayData->fetchAll();
-    $test = print_r($onerow, true);
-    return "<pre>$test</pre>";
+    $displayData = $displayJobData->displayJobData();
+    $showData = 
+    //$allData=$diplayData->fetchAll(PDO::FETCH_ASSOC);
+ 	//$allData = $diplayData->fetchAll();
+   
   /*  $displayJobData = include_once "views/display-job-data-html.php";
 	return $displayJobData ;*/
 
--- views/display-job-data-html.php
+++ views/display-job-data-html.php
@@ -1,5 +1,28 @@
 <?php 
-    return "
+    $display_html = "
+                    <div class='row'>
+            <div class='table-responsive display_table'>
+                <table class='table table-bordered table-hover' >
+                    <thead>
+                        <tr class='success'>
+                            <th style='text-align:center;'>Sr.No</th>
+                            <th style='text-align:center;'>File Name</th>
+                            <th style='text-align:center;'>Detail View</th>
+                            <th style='text-align:center;'>Edit</th>
+                            <th style='text-align:center;'>Delete</th>                            
+                        </tr>
+                    </thead>
+                    <tbody style='text-align:center;'>";
+    $display_html .= "
+                    <tr class='warning'>
+                        <td>$allData</td>
+                        <td>Simple File Name </td>
+                        <td><a class='btn btn-default'>Click To View</a></td>
+                        <td><a class='btn btn-success'>Click To Edit</a></td>
+                        <td><a class='btn btn-danger'>Click To Delete</a></td>
+                    </tr>
+    ";
+    /*return "
         <div class='row'>
           	<div class='table-responsive display_table'>
             	<table class='table table-bordered table-hover' >
@@ -24,9 +47,7 @@
 				</table>
 
             </div>
-          </div>
-    
-    ";
+          </div>";*/
 
 
 ?>
