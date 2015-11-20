7d2232e7901689ec04e9398409d86a2ed5871714
minnkoaung <minnkoaung@gmail.com>
2015-11-20 17:13:59 +0630

    add confirm model

--- views/display-job-data-html.php
+++ views/display-job-data-html.php
@@ -20,8 +20,28 @@
             $dispalyTableData .= "<td>".$row['file_url']."</td>";
             $dispalyTableData .= "<td><a href='index.php?page=detail_job_data&amp;id=".$row['id']."' class='btn btn-success'>Click To View</a></td>";
             $dispalyTableData .= "<td><a href='index.php?page=edit_job_data&amp;id=".$row['id']."' class='btn btn-warning'>Click To Edit</a></td>";
-            $dispalyTableData .= "<td><a href='index.php?page=delete_job_data&amp;id=".$row['id']."' class='btn btn-danger deleteJobData'>Click To Delete</a></td>";
+            $dispalyTableData .= "<td><a  class='btn btn-danger deleteJobData'  data-toggle='modal' data-target='#myModal'>Click To Delete</a></td>";
             $dispalyTableData .= "</tr>";
+            $dispalyTableData.= "<!-- Button trigger modal -->
+                                                                <!-- Modal -->
+                                                                <div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+                                                                  <div class='modal-dialog' role='document'>
+                                                                    <div class='modal-content'>
+                                                                      <div class='modal-header'>
+                                                                        <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                                                                        <h4 class='modal-title' id='myModalLabel'>Are You sure to Delete?</h4>
+                                                                      </div>
+                                                                      <div class='modal-body'>
+                                                                        <h4> You are About To Deleting <span style='color:red'>".$row['name']."</span> </h4>
+                                                                        <p style='color:red;'> Deleted Data will not recover agin.</p>
+                                                                      </div>
+                                                                      <div class='modal-footer'>
+                                                                        <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                                                                        <a href='admin.php?page=delete_job_data&amp;id=".$row['id']."' class='btn btn-danger'>Delete</a>
+                                                                      </div>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>";
         }
         $dispalyTableData .= " </tbody></table></div></div>";
         return $dispalyTableData;      
