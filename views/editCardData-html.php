4fd1059e125756760fb61718385181903a859d5a
minnkoaung <minnkoaung@gmail.com>
2015-11-20 19:26:09 +0630

    marge 01

--- views/editCardData-html.php
+++ views/editCardData-html.php
@@ -14,11 +14,39 @@ while($row=$test->fetch()){
                           <input type='text' class='form-control' name='item_name' value='".$row['item_name']."'>
                       </div>
                       <div class='form-group'>
-                          <label for='item_name'>Item Name</label>
-                          <input type='text' class='form-control' name='item_name' value='".$row['item_name']."'>
+                          <label for='year'>Year of Manufacture</label>
+                          <select class='form-control' name='year'>
+		                        <option>".$row['year']."</option>
+		                        <option>2014</option>
+		                        <option>2015</option>
+		                        <option>2016</option>
+		                        <option>2017</option>
+		                        <option>2018</option>
+		                        <option>2019</option>
+		                        <option>2020</option>
+		                        <option>2021</option>
+		                        <option>2022</option>
+		                  </select>
                       </div>
+                       <div class='form-group'>
+                          <label for='product_no'>Product No</label>
+                          <input type='text' class='form-control' name='product_no' value='".$row['product_no']."'>
+                      </div>
+                      <div class='form-group'>
+		                  <label for='product_of' >Product of</label>
+		                   <select class='form-control' name='product_of'>
+		                        <option>".$row['product_of']."</option>
+		                        <option>DKA</option>
+		                        <option>Nay La</option>
+		                        <option>Paper House</option>
+		                        <option>Shwe Padauk</option>
+		                        <option>Tun Naung</option>
+		                        <option>Tun Oo</option>
+		                        <option>Thandar Kyaw</option>
+		                  </select>
+		                </div>
 
-                    ";
+                      ";
 }
 $editOutputView .= "
                          <input type='submit' class='btn btn-success' name='updateJobData' value='Update'>
